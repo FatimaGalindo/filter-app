7020bcba1ad6175362e213780281b0e9b4bcbd5a
Fatima Galindo <fatima_gali.vilca@hotmail.com>
2020-09-07 00:49:08 -0500

    Updates

---
+++ precache-manifest.eed685be976f1596cf48023bfbbba63c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389ea855aed052241313892a55a47f77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21971a78cbd5fa06c40d",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4269db9047edd1ac439",
+    "url": "./static/js/2.c551f184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c551f184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21971a78cbd5fa06c40d",
+    "url": "./static/js/main.bad6de23.chunk.js"
+  },
+  {
+    "revision": "58fe76f654c37f3f9d37",
+    "url": "./static/js/runtime-main.84f5607a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FatimaGalindo/filter-app/precache-manifest.c96edb53bff334ba0d54b5ab0b538f54.js"
+  "./precache-manifest.eed685be976f1596cf48023bfbbba63c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FatimaGalindo/filter-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
