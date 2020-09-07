0eaa869a021120939e1c97257f4d62fde8232e7a
Fatima Galindo <fatima_gali.vilca@hotmail.com>
2020-09-07 00:21:10 -0500

    Updates

---
+++ precache-manifest.c96edb53bff334ba0d54b5ab0b538f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfe2dfed911eedefb48903528b5d231",
+    "url": "/FatimaGalindo/filter-app/index.html"
+  },
+  {
+    "revision": "1b70b4e375d780de66db",
+    "url": "/FatimaGalindo/filter-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4269db9047edd1ac439",
+    "url": "/FatimaGalindo/filter-app/static/js/2.c551f184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FatimaGalindo/filter-app/static/js/2.c551f184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b70b4e375d780de66db",
+    "url": "/FatimaGalindo/filter-app/static/js/main.78028e54.chunk.js"
+  },
+  {
+    "revision": "c9481cdd22892f7b17ea",
+    "url": "/FatimaGalindo/filter-app/static/js/runtime-main.0c9024e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FatimaGalindo/filter-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/FatimaGalindo/filter-app/precache-manifest.c96edb53bff334ba0d54b5ab0b538f54.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FatimaGalindo/filter-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
